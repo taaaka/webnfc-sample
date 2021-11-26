32327a7aa98bf67abb9370b7a2f20610abc71ad3
taaaka <Rw.taaaka@gmail.com>
2021-11-26 22:38:16 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0)
+
+const handleClick = () => {
+  count.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello, Nuxt 3!</h1>
+    <button @click="handleClick">Count up {{count}}</button>
+  </div>
+</template>
